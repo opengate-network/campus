b9b88695659328276a6aa84cb8115bc0b7ffaccc
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2023-04-27 19:09:36 +0200

    add past tickets

--- lib/pages/wallet/view/ticket_view.dart
+++ lib/pages/wallet/view/ticket_view.dart
@@ -2,20 +2,26 @@ import 'package:campus/pages/wallet/cubit/wallet_cubit.dart';
 import 'package:campus/pages/wallet/view/wallet_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pretty_qr_code/pretty_qr_code.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: BlocBuilder<WalletCubit, WalletState>(
-        builder: (context, state) {
-          return Column(
-            children: List.generate(
-              10,
-              (index) => WalletCard(
+    return CustomScrollView(slivers: [
+      _showActualTickets(),
+      _showPastTicketHeader(context),
+      _showPastTickets(),
+    ]);
+  }
+
+  Widget _showActualTickets() {
+    return BlocBuilder<WalletCubit, WalletState>(
+      builder: (context, state) {
+        return SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return WalletCard(
                 title: const Text('Kfet de Noël'),
                 subtitle: const Text('12 mars 2023 - 18h'),
                 code: '4q4dqz4dqz524dqz654d',
@@ -26,11 +32,52 @@ class TicketView extends StatelessWidget {
                 onTap: index == state.ticketExpandedIndex
                     ? null
                     : () => context.read<WalletCubit>().expandTicket(index),
+              );
+            },
+            childCount: 10,
+          ),
+        );
+      },
+    );
+  }
 
-              ),
-            ),
-          );
-        },
+  Widget _showPastTickets() {
+    return BlocBuilder<WalletCubit, WalletState>(
+      builder: (context, state) {
+        return SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final offsetIndex = index + 10;
+
+              return WalletCard(
+                title: const Text('Kfet de Noël'),
+                subtitle: const Text('12 mars 2023 - 18h'),
+                code: '4q4dqz4dqz524dqz654d',
+                imageProvider: const NetworkImage(
+                  'https://googleflutter.com/sample_image.jpg',
+                ),
+                expanded: offsetIndex == state.ticketExpandedIndex,
+                onTap: offsetIndex == state.ticketExpandedIndex
+                    ? null
+                    : () =>
+                        context.read<WalletCubit>().expandTicket(offsetIndex),
+              );
+            },
+            childCount: 10,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _showPastTicketHeader(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20).copyWith(top: 10),
+        child: Text(
+          'Tickets passés',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
       ),
     );
   }
