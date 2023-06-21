a1493ef868ae9d83ef866e45b755bb1d057cc7f9
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2023-06-21 10:30:02 +0200

    change OpenGate logo on login page

--- lib/pages/login/view/login_view.dart
+++ lib/pages/login/view/login_view.dart
@@ -11,24 +11,59 @@ class LoginView extends StatelessWidget {
     return SafeArea(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
-            child: MainAppbarLogo(
-              width: null,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List<Widget>.generate(
+                  5,
+                  (index) => Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 3),
+                    child: MainAppbarLogo(
+                      width: null,
+                      opacity: AlwaysStoppedAnimation(
+                          ((2 - index).abs() * -1 / 4) + 1),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 25),
-              child: SizedBox(
-                width: double.infinity,
-                child: FilledButton(
-                  onPressed: () => context.read<LoginCubit>().login(context),
-                  child: const Text('Se connecter'),
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Découvrez le campus avec nous',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    height: 0,
+                  ),
                 ),
-              ),
+                const SizedBox(height: 30),
+                const Text(
+                  'Campus vous accompagne dans votre vie étudiante à l\'université Gustave Eiffel',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: FilledButton(
+                    onPressed: () => context.read<LoginCubit>().login(context),
+                    child: const Text('Se connecter'),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
